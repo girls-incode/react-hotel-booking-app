01525a829c4c0d634c782d618af414b649bf5350
Angelescu Violeta <violeta@girlsincode.com>
2020-12-12 14:06:09 +0100

    responsive search, fix calendar, add bootstrap icons

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -10,35 +10,47 @@ function Search() {
     const [data, dispatch] = useSearchValue();
     const [startDate, setStartDate] = useState(new Date(data.checkin));
     const [endDate, setEndDate] = useState(new Date(data.checkout));
+    const today = new Date();
     let newData = {};
 
     const eventhandler = res => Object.assign(newData, res);
 
+    const changeDate = (date) => {
+        const nextDate = new Date(date);
+        nextDate.setDate(date.getDate() + 1);
+        setStartDate(date);
+        setEndDate(nextDate);
+    }
+
+    const CustomInput = ({ value, onClick }) => (
+        <div className='calendar' onClick={onClick}>
+            <input type='text' className="form-item" value={value} />
+            <i class="bi bi-calendar-week"></i>
+        </div>
+    );
+
     return (
         <section className='search d-flex align-items-center'>
             <div className='search-wrapper w-100 my-3'>
-                <div className='container py-3 d-flex justify-content-center align-items-center'>
+                <div className='container py-3 search-content'>
                     <DatePicker
                         dateFormat='dd/MM/yyyy'
                         selected={startDate}
-                        onChange={date => setStartDate(date)}
-                        selectsStart
+                        onChange={changeDate}
                         startDate={startDate}
-                        endDate={endDate}
-                        className='mx-3'
+                        minDate={today}
+                        customInput={<CustomInput />}
                     />
                     <DatePicker
                         dateFormat='dd/MM/yyyy'
                         selected={endDate}
                         onChange={date => setEndDate(date)}
-                        selectsEnd
                         startDate={endDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        className='mx-3'
+                        minDate={endDate}
+                        customInput={<CustomInput />}
                     />
-                    <SelectList name='adults' text='Adults' css='mx-3' start={1} num={data.adults} onChange={eventhandler} />
-                    <SelectList name='children' text='Children' css='mx-3' num={data.children} onChange={eventhandler} />
+                    <SelectList name='adults' text='Adults' css='form-item' start={1} num={data.adults} onChange={eventhandler} />
+                    <SelectList name='children' text='Children' css='form-item' num={data.children} onChange={eventhandler} />
                     <button className='btn btn-primary' onClick={() => dispatch({
                         type: 'changeSearch',
                         payload: {
